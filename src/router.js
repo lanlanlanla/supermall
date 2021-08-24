af18d18bb4af83a4e80cced55351cb9cae1a7bf4
Coder-Lang <3294728945@qq.com>
2021-08-24 21:49:07 +0800

    '初始化项目‘

---
+++ src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const Home = () => import('@/views/home/Home')
+const Category = () => import('@/views/category/Category')
+const Cart = () => import('@/views/cart/Cart')
+const Profile = () => import('./views/profile/Profile')
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: Category
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
+
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
+
+    }
+
+  ]
+})
