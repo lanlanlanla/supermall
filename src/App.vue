e01245362db56093c5c16465d1903b93f07861c9
Coder-Lang <3294728945@qq.com>
2021-08-31 09:24:10 +0800

    'fix'

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   </div>
 </template>
 <script>
-import MainTabBar from './components/common/mainTabbar/MainTabbar'
+import MainTabBar from 'components/content/mainTabbar/MainTabbar'
 export default {
   components: {
     MainTabBar
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,41 @@
 <template>
-<div> <h2>首页</h2></div>
+<div id="home">
+  <nav-bar class="home-nav">
+    <template v-slot:center>
+      购物街
+    </template>
+  </nav-bar>
+</div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import { getHomeMultidata } from 'network/home'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  },
+  data () {
+    return {
+      banner: [],
+      recommends: []
+    }
+  },
+  created () {
+    getHomeMultidata().then((res) => {
+      console.log(res)
+      this.banner = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color:#ffffff;
+}
 </style>
